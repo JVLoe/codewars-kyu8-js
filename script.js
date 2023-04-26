34fa6043ad492b1837beae1c0858ab183a620b68
Johanna Loe <108595341+JVLoe@users.noreply.github.com>
2023-04-26 21:26:36 +0100

    added some kyu 8 challenge solutions

--- script.js
+++ script.js
@@ -1,165 +1,306 @@
-/**
- * Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
- *
- * Examples
- * Input: [1, 5.2, 4, 0, -1]
- * Output: 9.2
- *
- * Input: []
- * Output: 0
- *
- * Input: [-2.398]
- * Output: -2.398
- *
- * Assumptions
- * You can assume that you are only given numbers.
- *     You cannot assume the size of the array.
- *     You can assume that you do get an array and if the array is empty, return 0.
- * What We're Testing
- * We're testing basic loops and math operations. This is for beginners who are just learning loops and math operations.
- * Advanced users may find this extremely easy and can easily write this in one line.
- */
-
-// my solution
-function sum (numbers) {
-    "use strict";
-    let total = 0;
-    for (let i = 0; i < numbers.length; i += 1) {
-        total += numbers[i];
+// Transportation on vacation
+const rentalCarCost = d => {
+    if (d >= 1 && d < 3){
+        return d * 40;
+    } else if (d >= 3 && d < 7) {
+        return (d * 40) - 20;
+    } else if (d >= 7) {
+        return (d * 40) - 50;
     }
-    if (total > 0 | total < 0) {
-        return total;
+}
+
+// Count by X
+const countBy = (x, n) => {
+    let result = [];
+    for (let i = 1; i <= n; i ++){
+        result.push(x * i);
+    }
+    return result;
+}
+
+// Switch It Up!
+
+const switchItUp = number => ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][number]
+
+const switchItUp2 = number => {
+    const words = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+    return words[number];
+}
+
+// Sum of positive
+
+const positiveSum = arr => {
+    const positiveArr = arr.filter(n => n > 0);
+    return positiveArr.reduce((a, b) => a + b, 0);
+}
+
+// Binary Addition
+function addBinary(a,b) {
+    return (a+b).toString(2)
+}
+
+// Descending Order
+
+function descendingOrder(n){
+    return parseInt(String(n).split('').sort().reverse().join(''))
+}
+
+function descendingOrder2(n){
+    let decend = n.toString().split('').sort().reverse().join('');
+    return Number(decend)
+}
+
+// The Feast of Many Beasts
+
+function feast(beast, dish) {
+    return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+}
+
+function feast2(beast, dish) {
+  beast = beast.split('');
+  let beasty = beast.shift();
+  beasty += beast.pop()
+
+  dish = dish.split('');
+  let dishy = dish.shift();
+  dishy += dish.pop();
+
+  if (beasty === dishy) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function feast3(beast, dish) {
+    beast = beast.split('');
+    let beasty = beast.shift();
+    beasty += beast.pop()
+
+    dish = dish.split('');
+    let dishy = dish.shift();
+    dishy += dish.pop();
+
+    if (beasty === dishy) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Cat years, Dog years
+
+const humanYearsCatYearsDogYears = (humanYears) => {
+    let cat = 0;
+    let dog = 0;
+
+    if (humanYears === 1) {
+        cat = 15;
+        dog = 15;
+    }
+
+    if (humanYears === 2) {
+        cat = 24;
+        dog = 24;
+    }
+
+    if (humanYears > 2) {
+
+        cat = (humanYears - 2) * 4 + 24;
+        dog = (humanYears - 2) * 5 + 24;
+    }
+
+    return [humanYears, cat, dog]
+}
+
+
+// Simple multiplications
+
+const simpleMultiplication = number => number % 2 === 0 ? number * 8 : number * 9;
+
+// Calculate average
+
+const findAverage = array => {
+    return array.length === 0 ? 0 : array.reduce((a, b)=> a + b, 0)/array.length
+}
+
+const findAverage1 = array => {
+    let total = 0;
+    for (let i = 0; i < array.length; i ++) {
+        total += array[i];
+    }
+    if (array.length > 0) {
+        return total / array.length;
     } else {
         return 0;
     }
-};
-
-// other solution
-function sum(numbers) {
-    return numbers.reduce((a, b) => a + b, 0);
 }
 
-/**
- * Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
- *
- * For example (Input -> Output):
- *
- * 2 -> 3 (1 + 2)
- * 8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
- */
+// Is it a palindrome?
 
-// my solution - for every number that is less than and equal to num, add 1
-// if num = 2: ill start at 0. Then as 1. then add another 1, which equals 2. then add 1 and 2.
-var summation = function (num) {
-    let sum = 0
-    for(let i = 0; i <= num; i++) {
-        sum += i
-    }
-    return sum
-}
+const isPalindrome = x => x.toLowerCase().split('').reverse().join('') == x.toLowerCase();
 
-/*
-* Write function bmi that calculates body mass index (bmi = weight / height2).
-
-if bmi <= 18.5 return "Underweight"
-
-if bmi <= 25.0 return "Normal"
-
-if bmi <= 30.0 return "Overweight"
-
-if bmi > 30 return "Obese"
-*
-* */
-function bmi(weight, height) {
-    let bmi = weight / (height * height);
-    if (bmi <= 18.5) {
-        return "Underweight";
-    } else if (bmi <= 25) {
-        return "Normal";
-    } else if (bmi <= 30) {
-        return "Overweight";
+const isPalindrome1 = x => {
+    const lowercase = x.toLowerCase();
+    const reverse = lowercase.split('').reverse().join('');
+    if (lowercase === reverse){
+        return true;
     } else {
-        return "Obese";
+        return false;
     }
 }
 
-/*
-*
-* You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
-* If it is a square, return its area. If it is a rectangle, return its perimeter.
+// Double Char
 
-* Example(Input1, Input2 --> Output):
-* 6, 10 --> 32
-* 3, 3 --> 9
-* */
+const doubleChar = (str) => str.split("").map(char => char + char).join("");
 
-// my solution
+function doubleChar1(str) {
+    const arr = str.split('').map(char => {
+        return char + char;
+    })
+    return arr.join('')
+}
 
-const areaOrPerimeter = function(l , w) {
-    if (l === w) {
-        return l * w;
+// Testing 1-2-3
+
+const number = array => {
+    return array.map(function (line, index) {
+        return (index + 1) + ": " + line;
+    });
+}
+
+function number1(array){
+    let newArray = [];
+    if (array.length === 0) {
+        return [];
     } else {
-        return (l + w) * 2;
+        for (let i = 0; i < array.length; i++){
+            newArray.push((i + 1) + ": " +array[i]);
+        }
     }
-};
-
-// other solution
-
-const areaOrPerimeter = function(l , w) {
-    return l == w ? l*w : 2*(l + w)
-};
-
-/**
- * Given a non-negative integer, 3 for example, return a string with a murmur:
- * "1 sheep...2 sheep...3 sheep...".
- * Input will always be valid, i.e. no negative integers.
- * */
-
-// solution
-var countSheep = function (num){
-    let str = "";
-    for (let i = 1; i <= num; i++) {
-        str += `${i} sheep...`;
-    }
-    return str;
+    return newArray
 }
 
-/**
- * Create a function that takes an integer as an argument and returns "Even" for even numbers
- * or "Odd" for odd numbers.
- * */
+// Volume of a Cuboid
 
-// my solution
-function evenOrOdd(number) {
-    if (number % 2 === 0) {
-        return "Even";
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        return length * width * height
+    }
+}
+
+// You only need one - Beginner
+const check = (a,x) => a.includes(x);
+
+function check1(a, x) {
+    if (a.includes(x)){
+        return true
     } else {
-        return "Odd";
+        return false
     }
 }
 
-// other solution
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
 
-function even_or_odd(number) {
-    return number % 2 ? "Odd" : "Even"
+String.prototype.toAlternatingCase = function () {
+    return this
+        .split("")
+        .map(
+            character =>
+                character === character.toUpperCase() ?
+                    character.toLowerCase() :
+                    character.toUpperCase()
+        )
+        .join("");
 }
 
-/**
- * Your goal is to create a function that removes the first and last characters of a string.
- * You're given one parameter, the original string.
- * You don't have to worry with strings with less than two characters.
- * */
+// Opposite number - broski abs
+const opposite = number => number < 0 ? Math.abs(number) : -number;
 
-// my solution
-function removeChar(str){
-    if (str.length >= 2) {
-        return str.substring(1, str.length - 1);
+// Disemvowel Trolls
+function disemvowel(str) {
+    const vowels = (/[aeiou]/gi);
+    return str.replace(vowels, '');
+}
+
+
+// A Needle in the Haystack
+function findNeedle(haystack) {
+    let needle = haystack.indexOf('needle');
+    return `found the needle at position ${needle}`;
+}
+
+// Convert number to reversed array of digits
+function digitize(n) {
+    return String(n).split('').map(Number).reverse()
+}
+
+function digitize1(n) {
+    let arr = String(n).split('').map((num) => {
+        return Number(num);
+    })
+    return arr.reverse();
+}
+
+// Counting sheep...
+
+function countSheeps(arrayOfSheeps) {
+    return arrayOfSheeps.filter(Boolean).length;
+}
+
+function countSheeps1(arrayOfSheep) {
+    let count = 0;
+    let total = 0;
+    for(let i = 0; i < arrayOfSheep.length; i++) {
+        if (arrayOfSheep[i] === true){
+            count ++;
+        } else if (arrayOfSheep === null || undefined) {
+            return false;
+        }
     }
-};
-
-// other solution
-
-function removeChar(str) {
-    return str.slice(1, -1);
+    return count;
 }
 
+// Convert boolean values to strings 'Yes' or 'No'.
+const boolToWord = bool => bool ? "Yes" : "No";
+
+const boolToWord1 = bool => bool === true ? "Yes" : "No";
+
+// Function 1 - hello world
+const greet = () => {
+    return "hello world!";
+}
+
+// Square(n) Sum
+function squareSum(numbers){
+    return numbers.reduce(function(sum, n){
+        return (n*n) + sum;
+    }, 0)
+}
+
+// Is he gonna survive?
+function hero(bullets, dragons){
+    return bullets / 2 >= dragons ? true : false;
+}
+
+function hero1(bullets, dragons){
+    return bullets / 2 === dragons || bullets / 2 > dragons ? true : false;
+}
+
+// Odd or Even?
+function oddOrEven(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    if (total % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+// Convert a Number to a String!
+function numberToString(num) {
+    return String(num);
+}
